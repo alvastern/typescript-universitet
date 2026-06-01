5056238b7909f65b0b1136b29f0297a62c340bec
alvastern <alva.stern02@gmail.com>
2026-06-01 10:36:51 +0200

    Skapade routing för headern + lade till json-fil för alla kurser

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Kurser } from './pages/kurser/kurser';
+import { Ramschema } from './pages/ramschema/ramschema';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Kurser },
+  { path: 'ramschema', component: Ramschema }
+];
--- src/app/pages/kurser/kurser.ts
+++ src/app/pages/kurser/kurser.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from "../../components/header/header";
 
 @Component({
   selector: 'app-kurser',
-  imports: [],
+  imports: [Header],
   templateUrl: './kurser.html',
   styleUrl: './kurser.scss',
 })
